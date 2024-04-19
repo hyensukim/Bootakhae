c2d3665f5814c2a77b0b0e702dd2f1ee0ad51c84
hyensukim <crazymin17@naver.com>
2024-04-19 21:42:45 +0900

    feat: 이메일 인증 구현

---
+++ src/main/java/com/bootakhae/webapp/user/controllers/EmailController.java
@@ -0,0 +1,45 @@
+package com.bootakhae.webapp.user.controllers;
+
+import com.bootakhae.webapp.user.services.EmailService;
+import com.bootakhae.webapp.user.vo.request.RequestEmailCheck;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/users/email")
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+
+    /**
+     * 인증 코드 메일 전송
+     */
+    @PostMapping("send")
+    public ResponseEntity<String> sendEmail(@RequestBody RequestEmailCheck request) {
+        emailService.sendMessage(request.getEmail());
+        return ResponseEntity.status(HttpStatus.OK).body("인증 코드가 발송되었습니다.");
+    }
+
+    /**
+     * 인증 코드 확인
+     */
+    @PostMapping("verify")
+    public ResponseEntity<String> verifyCode(@RequestBody RequestEmailCheck request) {
+        boolean isChecked = emailService.verifyCode(request.getEmail(), request.getCode());
+
+        if(isChecked) {
+            return ResponseEntity.status(HttpStatus.OK).body("인증이 완료되었습니다.");
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.OK).body("인증에 실패했습니다.");
+        }
+    }
+}
---
+++ src/main/java/com/bootakhae/webapp/user/vo/request/RequestEmailCheck.java
@@ -0,0 +1,12 @@
+package com.bootakhae.webapp.user.vo.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RequestEmailCheck {
+    @NotBlank(message="이메일을 입력 바랍니다.")
+    private String email; // 전송 이메일
+    @NotBlank(message="인증 코드를 입력 바랍니다.")
+    private String code; // 검증 코드
+}
