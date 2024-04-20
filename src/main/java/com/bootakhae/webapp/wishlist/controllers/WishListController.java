a0558e6223d82d4091d32aebf4b67a6856b371be
hyensukim <crazymin17@naver.com>
2024-04-20 21:08:31 +0900

    feat#5 : 찜 목록 전체 조회 기능 구현

--- src/main/java/com/bootakhae/webapp/wishlist/controllers/WishListController.java
+++ src/main/java/com/bootakhae/webapp/wishlist/controllers/WishListController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/wishes")
 @RequiredArgsConstructor
@@ -20,6 +22,13 @@ public class WishListController {
     @GetMapping("health-check")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("service is available");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseWishDto>> wishList(@RequestParam(defaultValue = "0") int nowPage,
+                                                          @RequestParam(defaultValue = "6") int pageSize){
+        List<ResponseWishDto> responseList = wishListService.getWishList(nowPage, pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(responseList);
     }
 
     @PostMapping
--- src/main/java/com/bootakhae/webapp/wishlist/services/WishListServiceImpl.java
+++ src/main/java/com/bootakhae/webapp/wishlist/services/WishListServiceImpl.java
@@ -8,10 +8,16 @@ import com.bootakhae.webapp.wishlist.dto.request.RequestWishDto;
 import com.bootakhae.webapp.wishlist.dto.response.ResponseWishDto;
 import com.bootakhae.webapp.wishlist.entities.WishListEntity;
 import com.bootakhae.webapp.wishlist.repositories.WishListRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -22,6 +28,9 @@ public class WishListServiceImpl implements WishListService{
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
 
+    /**
+     * 찜 등록
+     */
     @Transactional
     @Override
     public ResponseWishDto includeWish(RequestWishDto requestWishDto) {
@@ -44,6 +53,9 @@ public class WishListServiceImpl implements WishListService{
         return wishList.entityToDto();
     }
 
+    /**
+     * 수량 수정
+     */
     @Transactional
     @Override
     public ResponseWishDto updateQty(RequestWishDto requestWishDto) {
@@ -63,21 +75,31 @@ public class WishListServiceImpl implements WishListService{
         return wishList.entityToDto();
     }
 
+    /**
+     * 찜 삭제
+     */
     @Transactional
     @Override
     public void excludeWish(String userId, String productId) {
         UserEntity user = userRepository.findByUserId(userId).orElseThrow(
-                () -> new RuntimeException("찜 등록 : 로그인 후 이용 바랍니다.")
+                () -> new RuntimeException("찜 삭제 : 로그인 후 이용 바랍니다.")
         );
 
         ProductEntity product = productRepository.findByProductId(productId).orElseThrow(
-                () -> new RuntimeException("찜 등록 : 등록되지 않은 상품입니다.")
+                () -> new RuntimeException("찜 삭제 : 등록되지 않은 상품입니다.")
         );
 
         WishListEntity wishList = wishListRepository.findByProductAndUser(product, user).orElseThrow(
-                () -> new RuntimeException("찜 수량 변경 : 존재하지 않는 찜목록 입니다.")
+                () -> new RuntimeException("찜 삭제 : 존재하지 않는 찜목록 입니다.")
         );
 
         wishListRepository.delete(wishList);
+    }
+
+    @Override
+    public List<ResponseWishDto> getWishList(int nowPage, int pageSize) {
+        PageRequest pageRequest = PageRequest.of(nowPage, pageSize);
+        Page<WishListEntity> pageList = wishListRepository.findAll(pageRequest);
+        return pageList.stream().map(WishListEntity::entityToDto).toList();
     }
 }
